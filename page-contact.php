cbc196e14fca6af764a3948a26e8156df7d60730
zinpaihtet <zinpainghtet@qualymm.com>
2023-04-26 15:54:56 +0630

    contact page created

---
+++ page-contact.php
@@ -0,0 +1,33 @@
+<?php
+/*
+template name:contact
+ */
+
+get_header();
+?>
+
+<section class="sub-mv-section sub-bg">
+    <div class="container">
+        <div class="pc">
+            <?php echo the_breadcrumb(); ?>
+        </div>
+        <div class="sub-mv">
+            <h2 class="sub-mv-ttl">お問い合わせ</h2>
+            <span class="sub-mv-txt">Contact</span>
+        </div>
+    </div>
+</section>
+
+
+<section class="contact-info">
+    <div class="container">
+        <h2 class="h2-ttl h2-mb">禁煙外来について</h2>
+    </div>
+</section>
+
+
+
+<!-- alsdfla;sd -->
+
+<?php
+get_footer();
